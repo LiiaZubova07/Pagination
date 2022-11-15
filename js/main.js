9c21a9a3d9ad68dabc8663b4ffe66004258216c8
Liia Zubova <vardlitmo@gmail.com>
2022-11-15 20:25:31 +0300

    add: data js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-async function getData(){
-	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-	const data = await response.json();
-	return data;
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
 }
+
+async function main() {
+  const postsData = await getData();
+  let currentPage = 1;
+  let rows = 10;
+
+  function displayList(arrData, rowPerPage, page) {
+    const postsEl = document.querySelector('.posts');
+
+	 const start = rowPerPage * page;
+	 const end = start + rowPerPage;
+  }
+  function displayPagination() {}
+  function displayPaginationBtn() {}
+}
+main();
