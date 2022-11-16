d3309c54f8f40a9fa8e6fa147991d6e70f8b389d
Liia Zubova <vardlitmo@gmail.com>
2022-11-16 20:28:45 +0300

    add: diaplayPagination

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ async function main() {
   //информация, сколько стр отображать, какая страница
   function displayList(arrData, rowPerPage, page) {
     const postsEl = document.querySelector('.posts');
+	 //после того, как page1 будет приходить, будем уменьшать на 1
+	 page--;
 
     //начальное значение
     const start = rowPerPage * page;
@@ -20,22 +22,54 @@ async function main() {
     //получаю срез от начала до конца
     const paginatedData = arrData.slice(start, end);
 
-	 //создаю хтмл элементы и положу в postEl
-	 paginatedData.forEach((el) => {
-		//
-		const postEl = document.createElement('div');
-		//к div присвою класс .post
-		postEl.classList.add('post');
-		//в post положить элемент айди
-		postEl.innerText = `${el.title}`;
-		//добавляю пост в posts
-		postsEl.appendChild(postEl);
-	 });
+    //создаю хтмл элементы и положу в postEl
+    paginatedData.forEach((el) => {
+      //
+      const postEl = document.createElement('div');
+      //к div присвою класс .post
+      postEl.classList.add('post');
+      //в post положить элемент айди
+      postEl.innerText = `${el.title}`;
+      //добавляю пост в posts
+      postsEl.appendChild(postEl);
+    });
   }
-  function displayPagination() {}
-  function displayPaginationBtn() {}
+  //кол-во элементов на странице
+  function displayPagination(arrData, rowPerPage) {
+    const paginationEl = document.querySelector('.pagination');
+    //количество страниц
+    const pagesCount = Math.ceil(arrData.length / rowPerPage);
+    //чтобы класть список (li)
+    const ulEl = document.createElement('ul');
+    ulEl.classList.add('pagination__list');
 
-  displayList(postsData,rows, currentPage );
+    //пишу цикл, который запускается pagesCount раз
+    for (let i = 0; i < pagesCount; i++) {
+      //первая кнопка = 1; вторая кнопка = 2, ...
+      const liEl = displayPaginationBtn(i + 1);
+      //создаю li и аппендю в ulEl
+      ulEl.appendChild(liEl);
+    }
+    paginationEl.appendChild(ulEl);
+  }
+
+  function displayPaginationBtn(page) {
+    //
+    const liEl = document.createElement('li');
+    //навешиваю стиль
+    liEl.classList.add('pagination__item');
+    //в li положила номер страницы
+    liEl.innerText = page;
+
+    liEl.addEventListener('click', () => {
+      currentPage = page;
+      displayList(postsData, rows, currentPage);
+    });
+    return liEl;
+  }
+
+  displayList(postsData, rows, currentPage);
+  displayPagination(postsData, rows);
 }
 
 main();
